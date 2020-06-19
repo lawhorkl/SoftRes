f8d27a024ccc10f1368e891cc29f130769623d01
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2020-06-19 12:44:35 -0400

    Rework models

--- Models/PlayerClasses.cs
+++ Models/PlayerClasses.cs
@@ -2,6 +2,137 @@ using System.Collections.Generic;
 
 namespace SoftRes.Models
 {
+    public interface IPlayerClass
+    {
+        ClassRole Role { get; }
+        ClassSpec Spec { get; }
+        List<ClassSpec> Specs { get; }
+    }
+
+    public class Warrior : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.ProtectionWarrior,
+            ClassSpec.FuryWarrior,
+            ClassSpec.ArmsWarrior
+        };
+    }
+
+    public class Paladin : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.ProtectionPaladin,
+            ClassSpec.RetributionPaladin,
+            ClassSpec.HolyPaladin
+        };
+    }
+
+    public class Mage : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.FrostMage,
+            ClassSpec.ArcaneMage,
+            ClassSpec.FireMage
+        };
+    }
+
+    public class Warlock : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.AfflictionWarlock,
+            ClassSpec.DemonologyWarlock,
+            ClassSpec.DestructionWarlock
+        };
+    }
+
+    public class Druid : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+
+        };
+    }
+
+    public class Shaman : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.RestorationShaman,
+            ClassSpec.EnhancementShaman,
+            ClassSpec.ElementalShaman
+        };
+    }
+
+    public class Rogue : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.AssasinationRogue,
+            ClassSpec.CombatRogue,
+            ClassSpec.SubeltyRogue
+        };
+    }
+
+    public class Priest : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.HolyPriest,
+            ClassSpec.DisciplinePriest,
+            ClassSpec.ShadowPriest
+        };
+    }
+
+    public class Hunter : IPlayerClass
+    {
+        public ClassRole Role { get; }
+
+        public ClassSpec Spec { get; }
+
+        public List<ClassSpec> Specs { get; } = new List<ClassSpec>
+        {
+            ClassSpec.SurvivalHunter,
+            ClassSpec.MarksmanshipHunter,
+            ClassSpec.BeastMasteryHunter
+        };
+    }
+
     public enum ClassRole
     {
         Tank,
@@ -10,89 +141,35 @@ namespace SoftRes.Models
         Healer
     }
 
-    public interface IPlayerClass
+    public enum ClassSpec
     {
-        List<ClassRole> Roles { get; }
-    }
-
-    public class Warrior : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.Tank, 
-            ClassRole.MeleeDamage 
-        };
-    }
-
-    public class Paladin : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.Healer, 
-            ClassRole.Tank, 
-            ClassRole.MeleeDamage 
-        };
-    }
-
-    public class Mage : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.RangedDamage 
-        };
-    }
-
-    public class Warlock : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.RangedDamage 
-        };
-    }
-
-    public class Druid : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.Healer, 
-            ClassRole.Tank, 
-            ClassRole.MeleeDamage,
-            ClassRole.RangedDamage 
-        };
-    }
-
-    public class Shaman : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.Healer, 
-            ClassRole.MeleeDamage,
-            ClassRole.RangedDamage 
-        };
-    }
-
-    public class Rogue : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.MeleeDamage
-        };
-    }
-
-    public class Priest : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.Healer,
-            ClassRole.RangedDamage 
-        };
-    }
-
-    public class Hunter : IPlayerClass
-    {
-        public List<ClassRole> Roles { get; } = new List<ClassRole> 
-        { 
-            ClassRole.RangedDamage 
-        };
+        ProtectionWarrior,
+        ArmsWarrior,
+        FuryWarrior,
+        ProtectionPaladin,
+        RetributionPaladin,
+        HolyPaladin,
+        BalanceDruid,
+        FeralTankDruid,
+        FeralCatDruid,
+        RestorationDruid,
+        FireMage,
+        ArcaneMage,
+        FrostMage,
+        ShadowPriest,
+        HolyPriest,
+        DisciplinePriest,
+        AfflictionWarlock,
+        DestructionWarlock,
+        DemonologyWarlock,
+        MarksmanshipHunter,
+        SurvivalHunter,
+        BeastMasteryHunter,
+        RestorationShaman,
+        EnhancementShaman,
+        ElementalShaman,
+        CombatRogue,
+        SubeltyRogue,
+        AssasinationRogue,
     }
 }
